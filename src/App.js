17e6397d37eed48e3de6c4ba7226d397fc3f2496
sajida05 <sharb044@uottawa.ca>
2025-06-08 14:59:56 -0400

    ch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import TutorNavbar from './components/TutorNavbar';
 import Home from './pages/Home';
 import Tutors from './pages/Tutors';
